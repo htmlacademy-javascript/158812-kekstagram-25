045cf28d951861df314d011fa301ffcf50e0944a
Keks <keks@htmlacademy.ru>
2022-03-22 22:13:43 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/upload-file.js
+++ 7/js/upload-file.js
@@ -1,34 +1,24 @@
 //Модуль, отвечающий за загрузку изображения
 
-import {isEscapePressed} from './util.js';
 import {body} from './render-big-pictures.js';
-import {onUploadForm, hashtagInput} from './form-validation.js';
 
-const ImageEditor = document.querySelector('.img-upload__overlay');
-const textarea = ImageEditor.querySelector('.text__description');
+const imageEditor = document.querySelector('.img-upload__overlay');
 const uploadFile = document.querySelector('#upload-file');
 const uploadCancel = document.querySelector('#upload-cancel');
-const formUploadImage = document.querySelector('#upload-select-image');
 
 const cleanUploadFile = function () {
   uploadFile.value = '';
 };
 
 const openImageEditor = function () {
-  ImageEditor.classList.remove('hidden');
+  imageEditor.classList.remove('hidden');
   body.classList.add('modal-open');
 };
 
 const closeImageEditor = function () {
-  ImageEditor.classList.add('hidden');
+  imageEditor.classList.add('hidden');
   body.classList.remove('modal-open');
   cleanUploadFile();
-};
-
-const closeImageEditorEsc = function (evt) {
-  if (isEscapePressed(evt) && (textarea !== document.activeElement) && (hashtagInput !== document.activeElement)) {
-    closeImageEditor();
-  }
 };
 
 uploadFile.addEventListener('change', () => {
@@ -39,12 +29,4 @@ uploadCancel.addEventListener('click', () => {
   closeImageEditor();
 });
 
-document.addEventListener('keydown', () => {
-  closeImageEditorEsc();
-});
-
-formUploadImage.addEventListener('input', () => {
-  onUploadForm();
-});
-
-export {cleanUploadFile, openImageEditor, closeImageEditor, closeImageEditorEsc};
+export {cleanUploadFile, openImageEditor, closeImageEditor, imageEditor};
